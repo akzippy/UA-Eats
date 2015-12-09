95ac446033ce38b04334d3153ebd20c22719635d
Andrew Klenotic <ak186@zips.uakron.edu>
2015-12-09 16:23:12 -0500

    Added NSLocationWhenInUseUsageDescription in Plist.  Added MapView to ThirdViewController.

--- UA Eats/ThirdViewController.swift
+++ UA Eats/ThirdViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ThirdViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
